819eca6b3e90e32ccf4be83bf5be500b25a10744
Daniel <gtr512315@gmail.com>
2019-09-27 23:09:31 +0800

    Encryption code upload
    add caesar cipher, some testing code need elimination.

---
+++ Encrypt.c
@@ -0,0 +1,73 @@
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+//cipher declaration
+void caesar(char *plainText, int key);
+
+void playfair(char *plainText, char *key);
+
+void vernam(char *plainText, char *key);
+
+void row(char *plainText, char *key);
+
+void rail_fence(char *plainText, char *key);
+
+char cipherText[100];
+using namespace std;
+int main(int argc, char *argv[]){  // form = cipher key plaintext
+    if(strcmp(argv[1], "caesar") == 0){
+        // cout << "perform caesar\n";
+        // cout << "key = " << argv[2] << "\n";
+        // cout << "plaintext = " << argv[3];
+        caesar(argv[3], atoi(argv[2]));
+        for(int i =0; i <= strlen(argv[3])-1; i++){
+            cout << cipherText[i];
+        }
+    }else if(strcmp(argv[1], "playfair") == 0){
+        // cout << "perform playfair\n";
+        // cout << "key = " << argv[2] << "\n";
+        // cout << "plaintext = " << argv[3];
+        playfair(argv[3], argv[2]);
+    }else if(strcmp(argv[1], "vernam") == 0){
+        // cout << "perform vernam\n";
+        // cout << "key = " << argv[2] << "\n";
+        // cout << "plaintext = " << argv[3];
+        vernam(argv[3], argv[2]);
+    }else if(strcmp(argv[1], "row") == 0){
+        // cout << "perform row\n";
+        // cout << "key = " << argv[2] << "\n";
+        // cout << "plaintext = " << argv[3];
+        row(argv[3], argv[2]);
+    }else if(strcmp(argv[1], "rail_fence")== 0){
+        // cout << "perform rail_fence\n";
+        // cout << "key = " << argv[2] << "\n";
+        // cout << "plaintext = " << argv[3];
+        rail_fence(argv[3], argv[2]);
+    }else{
+        //do nothing
+    }
+}
+
+void caesar(char *plainText, int key){
+    for(int i=0; i <= strlen(plainText)-1; i++){
+        cipherText[i]=(plainText[i]-32+key)%91;
+    }
+}
+
+void playfair(char *plainText, char *key){
+
+}
+
+void vernam(char *plainText, char *key){
+
+}
+
+void row(char *plainText, char *key){
+
+}
+
+void rail_fence(char *plainText, char *key){
+
+}
