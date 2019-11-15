6ce97ebe231942a07d82851e2ad4bd2217f6d318
daniel <gtr512315@gmail.ocm>
2019-11-15 16:52:55 +0800

    test.ppm

--- hw3/ppm.py
+++ hw3/ppm.py
@@ -1,5 +1,9 @@
 from PIL import Image
 
-ppmPicture="./car.ppm"
-im = Image.open("./car_jpg.jpg")
-im.save(ppmPicture)
+# ppmPicture="./car.ppm"
+# im = Image.open("./car_jpg.jpg")
+# im.save(ppmPicture)
+
+ppmPicture = "./test.ppm"
+im = Image.open(ppmPicture)
+im.save('./test.png', "png")
