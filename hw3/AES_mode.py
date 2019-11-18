0e45cab19f880ea7e4311e935222f75b23673bdd
Huangjr <jr@nlplab.cc>
2019-11-18 16:44:22 +0800

    remove some code

--- hw3/AES_mode.py
+++ hw3/AES_mode.py
@@ -2,18 +2,6 @@ from Crypto.Cipher import AES
 from PIL import Image
 import sys, os
 
-# def data_generator(information, block_num):
-#     info = []
-#     for x in information:
-#         info.append(x)
-#         if len(info) == block_num:
-#             yield bytes(info)
-#             info = []
-#     # padding        
-#     if len(info) != 0:
-#         info.extend([0]*(block_num-len(info)))
-#         yield bytes(info)
-
 def data_generator(pixs, number):
     data = []
     for pix in pixs:
@@ -30,42 +18,44 @@ def pad(text, num):
     padding = num - (len(text) % num)
     return text + bytes([padding] * padding)
 
-# for AES_ECB_MODE
+
 size = 32
-key = b'iloveyou'
+key = b'11111111'
 ppmPicture = "./penguin.ppm"
 im = Image.open("./penguin.jpg")
 im.save(ppmPicture)
-cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB) 
 f = open("./penguin.ppm",'rb').read()
 ppm_type, size, max_color, pixs = f.split(b'\n', 3)
-f = open("./penguin_aes_ECB.ppm", "wb")
-f.write(ppm_type)
-f.write(b'\n')
-f.write(size)
-f.write(b'\n')
-f.write(max_color)
-f.write(b'\n')
-   
 
-for data in data_generator(pixs, size): 
+
+# for AES_ECB_MODE
+cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB) 
+f_ECB = open("./penguin_aes_ECB.ppm", "wb")
+f_ECB.write(ppm_type)
+f_ECB.write(b'\n')
+f_ECB.write(size)
+f_ECB.write(b'\n')
+f_ECB.write(max_color)
+f_ECB.write(b'\n')
+   
+for data in data_generator(pixs, size):
     ciphertext = cipher_ECB.encrypt(data)
-    f.write(bytes(ciphertext))
-f.close()
+    f_ECB.write(bytes(ciphertext))
+f_ECB.close()
 
 # for AES_CBC_MODE
 iv = os.urandom(16)
 cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) 
-f = open("./penguin.ppm",'rb').read()
-f = open("./penguin_aes_CBC.ppm", "wb")
-f.write(ppm_type)
-f.write(b'\n')
-f.write(size)
-f.write(b'\n')
-f.write(max_color)
-f.write(b'\n')
+f_CBC = open("./penguin.ppm",'rb').read()
+f_CBC = open("./penguin_aes_CBC.ppm", "wb")
+f_CBC.write(ppm_type)
+f_CBC.write(b'\n')
+f_CBC.write(size)
+f_CBC.write(b'\n')
+f_CBC.write(max_color)
+f_CBC.write(b'\n')
 
 for data in data_generator(pixs, size): 
     ciphertext = cipher_CBC.encrypt(data)
-    f.write(bytes(ciphertext))
-f.close()
+    f_CBC.write(bytes(ciphertext))
+f_CBC.close()
