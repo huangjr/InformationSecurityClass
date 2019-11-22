a007d76c0878b68c0666f917daccf39ed5e6ac6e
daniel <gtr512315@gmail.ocm>
2019-11-22 21:01:55 +0800

    fix encrypt cbc mode
    the previous version follow the wrong implementation. so i fix it

--- hw3/AES_mode.py
+++ hw3/AES_mode.py
@@ -9,8 +9,8 @@ def data_generator(pixs, number):
     for pix in pixs:
         data.append(pix) 
         if len(data) == number:
-            yield bytes(data)
-            data = []
+            yield bytes(data)   # here would be execute every number time
+            data = []           # clear the buffer
     # padding
     if len(data) != 0:
         yield pad(bytes(''.join( str(x) for x in data), encoding = 'utf-8'), number)
@@ -18,6 +18,23 @@ def data_generator(pixs, number):
 def pad(text, num):
     padding = num - (len(text) % num)
     return text + bytes([padding] * padding)
+
+def CBC_encrypt(text, key, iv):
+    cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB)
+    # xor text with iv
+    data = ""
+    for a,b in zip(text, iv):
+        c = format(a^b, '#04x')
+        # make the string:0x00 form, or some informaiton would lose
+        data += c[2:]
+    print(data)
+    cText = bytes.fromhex(data)
+    print(cText)
+    # encrypt text with key in ECB mode
+    cText = cipher_ECB.encrypt(cText)
+    # return text for next round's iv
+    print(cText,"\n")
+    return cText
 
 def prepare(file, text):
     key = bytes(text, encoding = "utf8")
@@ -40,9 +57,9 @@ def prepare(file, text):
     for data in data_generator(pixs, 16):
         try:
             ciphertext = cipher_ECB.encrypt(data)
+            f_ECB.write(bytes(ciphertext))
         except:
             print(data)
-        f_ECB.write(bytes(ciphertext))
     f_ECB.close()
 
     ppmPicture = "./" + file + "_aes_ECB.ppm"
@@ -50,8 +67,9 @@ def prepare(file, text):
     im.save("./" + file + "_aes_ECB.jpg", 'JPEG')
 
     # for AES_CBC_MODE
-    iv = os.urandom(16)
-    cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) 
+    # iv should be explicit defined
+    iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+    # cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) # here should use ECB mode, iv=key?
     f_CBC = open("./" + file + "_aes_CBC.ppm", "wb")
     f_CBC.write(ppm_type)
     f_CBC.write(b'\n')
@@ -62,10 +80,11 @@ def prepare(file, text):
 
     for data in data_generator(pixs, 16): 
         try :
-            ciphertext = cipher_CBC.encrypt(data)
+            ciphertext = CBC_encrypt(data, key, iv)
+            iv = ciphertext
+            f_CBC.write(bytes(ciphertext))
         except:
-            print(data)
-        f_CBC.write(bytes(ciphertext))
+            pass
     f_CBC.close()
 
     ppmPicture = "./" + file + "_aes_CBC.ppm"
