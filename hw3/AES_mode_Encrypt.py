f6eefbe5b54d87c0fc3e62a41052ffa0116bbb46
daniel <gtr512315@gmail.ocm>
2019-11-27 18:38:50 +0800

    test remove, fix diy_encrypt

--- hw3/AES_mode_Encrypt.py
+++ hw3/AES_mode_Encrypt.py
@@ -42,6 +42,17 @@ def XOR(text, iv):
         data += c[2:]
     cText = bytes.fromhex(data)
     return cText
+    
+def DIY_encrypt(text, key, iv):
+    a = text.hex()[:8]
+    b = text.hex()[8:]
+    cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB)
+    a_c = cipher_ECB.encrypt(bytes.fromhex(a))
+    b_c = cipher_ECB.encrypt(bytes.fromhex(b))
+    a_cipher = XOR(a_c, iv)
+    b_cipher = XOR(b_c, iv)
+    new_iv = XOR(a_cipher, b_cipher)
+    return a_cipher + b_cipher, new_iv
 
 def prepare(file, text):
     key = bytes(text, encoding = "utf8")
@@ -51,52 +62,52 @@ def prepare(file, text):
     f = open(ppmPicture,'rb').read()
     ppm_type, size, max_color, pixs = f.split(b'\n', 3)
 
-    # #### for AES_ECB_MODE
-    # cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB) 
-    # f_ECB = open("./" + file.split(".")[0] + "_Encrypt_ECB.ppm", "wb")
-    # f_ECB.write(ppm_type)
-    # f_ECB.write(b'\n')
-    # f_ECB.write(size)
-    # f_ECB.write(b'\n')
-    # f_ECB.write(max_color)
-    # f_ECB.write(b'\n')
+    #### for AES_ECB_MODE
+    cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB) 
+    f_ECB = open("./" + file.split(".")[0] + "_Encrypt_ECB.ppm", "wb")
+    f_ECB.write(ppm_type)
+    f_ECB.write(b'\n')
+    f_ECB.write(size)
+    f_ECB.write(b'\n')
+    f_ECB.write(max_color)
+    f_ECB.write(b'\n')
     
-    # for data in data_generator(pixs, 16):
-    #     try:
-    #         ciphertext = cipher_ECB.encrypt(data)
-    #     except:
-    #         print(data)
-    #     f_ECB.write(bytes(ciphertext))
-    # f_ECB.close()
+    for data in data_generator(pixs, 16):
+        try:
+            ciphertext = cipher_ECB.encrypt(data)
+        except:
+            print(data)
+        f_ECB.write(bytes(ciphertext))
+    f_ECB.close()
 
-    # ppmPicture = "./" + file.split(".")[0] + "_Encrypt_ECB.ppm"
-    # im = Image.open(ppmPicture)
-    # im.save("./" + file.split(".")[0] + "_Encrypt_ECB.jpg", 'JPEG')
+    ppmPicture = "./" + file.split(".")[0] + "_Encrypt_ECB.ppm"
+    im = Image.open(ppmPicture)
+    im.save("./" + file.split(".")[0] + "_Encrypt_ECB.jpg", 'JPEG')
 
-    # #### for AES_CBC_MODE
-    # # iv should be explicit defined
-    # iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
-    # # cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) # here should use ECB mode, iv=key?
-    # f_CBC = open("./" + file.split(".")[0] + "_Encrypt_CBC.ppm", "wb")
-    # f_CBC.write(ppm_type)
-    # f_CBC.write(b'\n')
-    # f_CBC.write(size)
-    # f_CBC.write(b'\n')
-    # f_CBC.write(max_color)
-    # f_CBC.write(b'\n')
+    #### for AES_CBC_MODE
+    # iv should be explicit defined
+    iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+    # cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) # here should use ECB mode, iv=key?
+    f_CBC = open("./" + file.split(".")[0] + "_Encrypt_CBC.ppm", "wb")
+    f_CBC.write(ppm_type)
+    f_CBC.write(b'\n')
+    f_CBC.write(size)
+    f_CBC.write(b'\n')
+    f_CBC.write(max_color)
+    f_CBC.write(b'\n')
 
-    # for data in data_generator(pixs, 16): 
-    #     try :
-    #         ciphertext = CBC_encrypt(data, key, iv)
-    #         iv = ciphertext
-    #     except:
-    #         print(data)
-    #     f_CBC.write(bytes(ciphertext))
-    # f_CBC.close()
+    for data in data_generator(pixs, 16): 
+        try :
+            ciphertext = CBC_encrypt(data, key, iv)
+            iv = ciphertext
+        except:
+            print(data)
+        f_CBC.write(bytes(ciphertext))
+    f_CBC.close()
 
-    # ppmPicture = "./" + file.split(".")[0] + "_Encrypt_CBC.ppm"
-    # im = Image.open(ppmPicture)
-    # im.save("./" + file.split(".")[0] + "_Encrypt_CBC.jpg" , 'JPEG')
+    ppmPicture = "./" + file.split(".")[0] + "_Encrypt_CBC.ppm"
+    im = Image.open(ppmPicture)
+    im.save("./" + file.split(".")[0] + "_Encrypt_CBC.jpg" , 'JPEG')
 
     #### for AES_DIY_MODE
     iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
@@ -126,18 +137,6 @@ def prepare(file, text):
     im = Image.open(ppmPicture)
     im.save("./" + file.split(".")[0] + "_Encrypt_DIY.jpg" , 'JPEG')
 
-def DIY_encrypt(text, key, iv):
-    a = text.hex()[:len(text.hex())//2]
-    b = text.hex()[len(text.hex())//2:]
-    cipher_ECB = AES.new(pad(key, 16), AES.MODE_ECB)
-    a_c = cipher_ECB.encrypt(bytes.fromhex(a))
-    b_c = cipher_ECB.encrypt(bytes.fromhex(b))
-    a_cipher = XOR(a_c, iv)
-    b_cipher = XOR(b_c, iv)
-    new_iv = XOR(a_cipher, b_cipher)
-    return a_cipher + b_cipher, new_iv
-
-    # for AES_DIY_MODE
     # iv should be explicit defined
     iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
     # cipher_CBC = AES.new(pad(key, 16), AES.MODE_CBC, iv) # here should use ECB mode, iv=key?
